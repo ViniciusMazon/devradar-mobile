8da1c03db2dbd0aee4912c06c256a813edcc643a
Vinicius Mazon <vnpmazon@gmail.com>
2020-02-11 08:57:04 -0300

    :rocket: add the mobile

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar, YellowBox } from 'react-native';
+import Routes from './src/routes';
+
+YellowBox.ignoreWarnings(['Unrecognized WebSocket connection option']);
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7"/>
+      <Routes />
+    </>
+  );
+}
